c7af0bf4991a671665d8975f5b08da53b3192dca
aeberle55 <aeberle1@utexas.edu>
2014-02-02 13:25:27 -0600

    toupper Changes
    Small alterations to toupper

--- Lab1/EE460N_Lab1/toupper.asm
+++ Lab1/EE460N_Lab1/toupper.asm
@@ -1,11 +1,10 @@
 ; George Netscher and Austin Eberle
 ; Convert string from lower to upper case
 	.ORIG	x3000
-A	.FILL	x4000
-	LEA	R0, A		; R0 contains string starting address
-	LDW	R0, R0, #0
-	ADD	R2, R0, #2	; R2 contains final address (x4002)
-LOOP	LDB	R4, R0, #2	; load char into R4
+	LEA	R0, A		; R0 contains location of string starting address
+	LDW R2, R0, #1	; R2 contains final address
+	LDW	R0, R0, #0	; R0 contains string starting address
+LOOP	LDB	R4, R0, #0	; load char into R4
 	ADD	R4, R4, #-16	
 	ADD	R4, R4, #-16	; subtract 32 to convert to uppercase
 	BRN	DONE		; negative means null char
@@ -15,4 +14,6 @@ LOOP	LDB	R4, R0, #2	; load char into R4
 	BRNZP	LOOP
 DONE	AND	R4, R4, #0	; put null char in R4
 	STB	R4, R2, #0	; append '/0' to output string
+	HALT
+A	.FILL	x4000
 	.END
