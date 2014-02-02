23c0b4d45551ceeda4340d47a58b92fc7ae22a63
aeberle55 <aeberle1@utexas.edu>
2014-02-02 14:31:42 -0600

    No .h allowed
    Stupid no .h files...

--- Lab1/EE460N_Lab1/Lab1.c
+++ Lab1/EE460N_Lab1/Lab1.c
@@ -6,11 +6,72 @@
 */
 
 /*
-EE460N Lab1
-2/2/14
+	EE460N Lab1
+	2/2/14
 */
 
-#include "Lab1.h"
+#include <stdio.h> /* standard input/output library */
+#include <stdlib.h> /* Standard C Library */
+#include <string.h> /* String operations library */
+#include <ctype.h> /* Library for useful character operations */
+#include <limits.h> /* Library for definitions of common variable type characteristics */
+
+typedef struct{
+	int location;
+	char* label;
+}Symbol;
+
+#define MAX_LINE_LENGTH 255
+#define MAX_SYMBOLS 255
+enum
+{
+   DONE, OK, EMPTY_LINE, END_OP
+};
+
+
+char *prgName   = NULL;	/*Name of Program */
+char *iFileName = NULL;	/*Input file name */
+char *oFileName = NULL;	/*Output file name */
+FILE* infile = NULL;		/*Input file stream */
+FILE* outfile = NULL;		/*Output file stream */
+int error=0;			/*0 if no error; otherwise error code 1-4 */
+int origin;			/*Starting point; set by .orig */
+int PC;				/*Program counter */
+int numSymbols = 0;	/*Number of labels in symbol table */
+Symbol table[255];		/*Array of symbol objects for symbol table */
+
+#define numCodes 31
+char* codes[numCodes] = {"add", "and", "br", "brn", "brz", "brp", "brnz", "brnp", "brzp", "brnzp", "halt", "jmp", "jsr", "jsrr", "ldb", "ldw", "lea", "nop",
+	"not", "ret", "lshf", "rshfl", "rshfa", "rti", "stb", "stw", "trap", "xor",".orig",".fill",".end"};
+
+#define numIllegal 4
+char* illegalLabel[numIllegal] = {"in","out","getc","putc"};
+
+int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char
+	** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4
+	);
+
+int toNum( char * pStr ); 
+
+Symbol* searchSymbol(char * sym);
+
+int RegNum(char * reg);
+
+int genOffset(int symbol_location, int maxDigits);
+
+int parseOpcode(char * opcode, int * steering);
+
+int isOpcode(char * code);
+
+void closeFiles();
+
+int readLabel(FILE * pInfile, char * pLine, char ** pLabel);
+
+void addLabel(char** label);
+
+
+void processOpcode( int code, int * steer, char * pArg1, char * pArg2, char * pArg3, char * pArg4);
+
 
 int main(int argc, char** argv)
 {
